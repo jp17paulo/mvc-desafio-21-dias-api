c5b412447cb258dbed490a6da2217e6a6478a228
jp17paulo <jp17paulo@terra.com.br>
2021-08-16 20:09:15 -0300

    Primeiro Commit

---
+++ Controllers/AlunosController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using mvc.Models;
+
+namespace mvc.Controllers
+{
+    public class AlunosController : Controller
+    {
+        private readonly ILogger<AlunosController> _logger;
+
+        public AlunosController(ILogger<AlunosController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("/alunos")]
+        public IActionResult Index()
+        {
+            return View(Aluno.Todos());
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
